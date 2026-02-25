d9168bb9a8da0833548d2f1f9fce632b55190a7b
Evelofdex <evelofdex@gmail.com>
2026-02-25 07:42:04 +0700

    coba pembuatan ascii berdasarkan input (bagian panjang)

---
+++ test_ascii.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int var_panjang, var_lebar;
+//mode
+bool isRunning = true;
+bool isValid = true;
+char var_ngulang;
+
+int main() {
+    isValid = true;
+    while (isRunning){
+        cout << "masukan panjang: ";
+        cin >> var_panjang;
+        // cout << "masukan lebar: ";
+        // cin >> var_lebar;
+        //kalkulasi
+        for (int i = 0; i < var_panjang; i++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        while (isValid){
+            cout << "lagi? (y/n): ";
+            cin >> var_ngulang;
+            if (var_ngulang == 'y' || var_ngulang == 'Y'){
+                isRunning = true;
+                isValid = false;
+            } else if (var_ngulang == 'n' || var_ngulang == 'N')
+            {
+                isRunning = false;
+                isValid = false;
+            } else {
+                cout << "input tidak valid";
+            }
+        }
+    }
+}
